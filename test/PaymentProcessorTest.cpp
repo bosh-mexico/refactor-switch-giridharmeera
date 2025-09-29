abff6ecb8622bd4970195b0f358313e6db6bb80c
Meera Giridhar (MS/EVW1-XC) <giridhar.meera@in.bosch.com>
2025-09-29 20:00:12 +0530

    adding testcases

---
+++ test/PaymentProcessorTest.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include "CreditCardProcessor.h"
+#include "PayPalProcessor.h"
+#include "GooglePayProcessor.h"
+#include "PaymentProcessorFactory.h"
+
+TEST(CreditCardProcessorTest, CanProcess) {
+    CreditCardProcessor processor;
+    EXPECT_NO_THROW(processor.process(100.0));
+}
+
+TEST(PayPalProcessorTest, CanProcess) {
+    PayPalProcessor processor;
+    EXPECT_NO_THROW(processor.process(200.5));
+}
+
+TEST(GooglePayProcessorTest, CanProcess) {
+    GooglePayProcessor processor;
+    EXPECT_NO_THROW(processor.process(300.75));
+}
+
+TEST(PaymentProcessorFactoryTest, CreatesValidProcessor) {
+    PaymentProcessorFactory factory;
+    auto paypal = factory.create("PayPal");
+    EXPECT_NO_THROW(paypal->process(50));
+
+    auto gpay = factory.create("GooglePay");
+    EXPECT_NO_THROW(gpay->process(75));
+
+    auto credit = factory.create("CreditCard");
+    EXPECT_NO_THROW(credit->process(100));
+}
+
+TEST(PaymentProcessorFactoryTest, ThrowsOnInvalidProcessor) {
+    PaymentProcessorFactory factory;
+    EXPECT_THROW(factory.create("Bitcoin"), std::invalid_argument);
+}
